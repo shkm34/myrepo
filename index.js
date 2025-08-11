a80a24a8fbad153d63ef04a84c79668c36647d76
shivam <shkm34@gmail.com>
2025-08-11 13:26:07 +0530

    navbar note

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function myFunction() {
   console.log("tisre baar");
   console.log("ek baar aur");
   console.log("footer note: This is a sample footer note.");
+  console.log("navbar");
 }
 myFunction();
